35c5a66da98a2f4a4eb78137114fe8b2fe73e020
mccormky <mccormky@bc.edu>
2015-05-06 13:42:34 -0400

    Update workoutArticles.php

--- workoutArticles.php
+++ workoutArticles.php
@@ -14,12 +14,13 @@ session_start();
 	<header>
 		<div class="navBar">
 			<ul>
-				<li class="home"><a href="http://cscilab.bc.edu/~mccormky/FinalProject/profilePage.php">Home</a></li>
-				<li class="workoutPlanner"><a href="http://cscilab.bc.edu/~mccormky/FinalProject/selectWorkouts.php">Workout Planner</a></li>
-				<li><a href="http://cscilab.bc.edu/~mccormky/FinalProject/workoutArticles.php">Workout Articles</a></li>
-				<li><a href="http://cscilab.bc.edu/~pridotka/project/leaderboard.php">Leader Board</a></li>
-				<li><a href="http://cscilab.bc.edu/~pridotka/project/find_gym.php">Gyms Near You</a></li><br>
-				<li class="loggedIn" style="font-size:10pt">Logged in As: <?php echo $_SESSION["UserName"] ?></li>
+				<li> <img src="workoutLogo.png" alt="logo" id="logo"/></li>
+  				<li class="home"><a href="http://cscilab.bc.edu/~mccormky/FinalProject/profilePage.php">Home</a></li>
+  				<li class="workoutPlanner"><a href="http://cscilab.bc.edu/~mccormky/FinalProject/selectWorkouts.php">Workout Planner</a></li>
+  				<li><a href="http://cscilab.bc.edu/~mccormky/FinalProject/workoutArticles.php">Workout Articles</a></li>
+  				<li><a href="http://cscilab.bc.edu/~pridotka/project/leaderboard.php">Leader Board</a></li>
+  				<li><a href="http://cscilab.bc.edu/~pridotka/project/find_gym.php">Gyms Near You</a></li>
+  				<li class="loggedIn" style="font-size:10pt">Logged in As: <?php echo $_SESSION["UserName"] ?></li>
   				<li><a style="font-size:10pt" href="http://cscilab.bc.edu/~pridotka/project/login.php">Log Out</a></li>
 			</ul>
 		</div>
@@ -35,9 +36,9 @@ session_start();
 	</div>
 </form>
 <?php
-
+	
 		workout_articles_DisplayForm();
-
+	
 ?>
 
 </body>
@@ -46,8 +47,8 @@ session_start();
 <?php
 
 function workout_articles_DisplayForm() {
-
-	 if(!isset($_GET['newArticles']) && !isset($_GET['trainingArticles'])
+	 
+	 if(!isset($_GET['newArticles']) && !isset($_GET['trainingArticles']) 
 	 	&& !isset($_GET['supplementArticles']) && !isset($_GET['nutritionArticles'])) {
 	 		echo "<h2 style='text-align:center;'>New Articles</h2>";
 		 	 $rss= new SimpleXMLElement(file_get_contents('http://www.bodybuilding.com/rss/articles'));
@@ -63,10 +64,10 @@ function workout_articles_DisplayForm() {
         		echo $item->description . "<br><br>\n";
         		echo "<hr></div>";
         		echo "</fieldset>";
-
+	 		
 			 }
 	 } else {
-
+	 
 	 if(isset($_GET['newArticles'])) {
 	 	echo "<h2 style='text-align:center;'>New Articles</h2>";
 	 	 $rss= new SimpleXMLElement(file_get_contents('http://www.bodybuilding.com/rss/articles'));
@@ -135,7 +136,7 @@ function workout_articles_DisplayForm() {
         	echo "</fieldset>";
         }
     }
-
+    
     }
 
 }
