056aef352d4da2acf8cc601e2e149fdfc1fdd1cc
pridotka <pridotka@bc.edu>
2015-05-06 12:12:14 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,42 +1,41 @@
 <?php
-//include('login.php'); // Includes Login Script
-
-//if(isset($_SESSION['login_user'])){
-//header("location: profile.php");
-//}
+//session_destroy();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>Login to Workouts</title>
+
 	<link rel="stylesheet" href="finalProject.css" />
 </head>
 <body>
-<div id="main">
-<h1>Login</h1>
 
+<h1>Login to Workouts!</h1>
+
+<fieldset style="width:300px; margin: auto">
 <div id="login">
-<h2>Login Form</h2>
 <form action="Login_Action.php" method="post">
 	<label>UserName :</label>
 		<input id="name" name="username" placeholder="username" type="text"><br>
 	<label>Password :</label>
-		<input id="password" name="password" placeholder="**********" type="password"><br>
-		<input name="submit" type="submit" value=" Login "><br><br>
+		<input id="password" name="password" placeholder="**********" type="password"><br><br>
+		<input name="submit" type="submit" value=" Login " class="button"><br><br>
 </form>
 </div>
-</div>
 
-	<fieldset><legend>Join Workouts!</legend>
+</fieldset>
+
+
+	<fieldset style="width:300px; margin: auto"><div><h3>Not A Member?</h3>
 		<form action="join.php" method="link">
-			 <input value="Join!" type="submit">
-		</form>
+			 <input value="Join!" type="submit" class="button">
+		</form></div>
 	</fieldset>
 
-	<fieldset><legend>I forgot my Password...</legend>
+	<fieldset style="width:300px; margin: auto"><div><h3>Forgot Your Password?</h3>
 		<form action="newPass.php" method="post">
-			 <input class="new" name = "new" value="Send Me A New Password!" type="submit">
-		</form>
+			 <input class="button" name = "new" value="Send Me A New Password!" type="submit">
+		</form></div>
 	</fieldset>
 
 
