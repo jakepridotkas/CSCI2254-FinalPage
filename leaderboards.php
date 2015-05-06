70b2aa645d49932cea780d3e305fcf1a9a7666c6
mccormky <mccormky@bc.edu>
2015-05-06 13:43:38 -0400

    Update leaderboards.php

--- leaderboards.php
+++ leaderboards.php
@@ -13,14 +13,14 @@ session_start();
   <header>
   		<div class="navBar">
   			<ul>
+  				<li> <img src="workoutLogo.png" alt="logo" id="logo"/></li>
   				<li class="home"><a href="http://cscilab.bc.edu/~mccormky/FinalProject/profilePage.php">Home</a></li>
   				<li class="workoutPlanner"><a href="http://cscilab.bc.edu/~mccormky/FinalProject/selectWorkouts.php">Workout Planner</a></li>
   				<li><a href="http://cscilab.bc.edu/~mccormky/FinalProject/workoutArticles.php">Workout Articles</a></li>
   				<li><a href="http://cscilab.bc.edu/~pridotka/project/leaderboard.php">Leader Board</a></li>
-  				<li><a href="http://cscilab.bc.edu/~pridotka/project/find_gym.php">Gyms Near You</a></li><br>
+  				<li><a href="http://cscilab.bc.edu/~pridotka/project/find_gym.php">Gyms Near You</a></li>
   				<li class="loggedIn" style="font-size:10pt">Logged in As: <?php echo $_SESSION["UserName"] ?></li>
   				<li><a style="font-size:10pt" href="http://cscilab.bc.edu/~pridotka/project/login.php">Log Out</a></li>
-
   			</ul>
   		</div>
 	</header>
@@ -36,8 +36,8 @@ session_start();
    	$db = connectToDB("pridotka");
 	$query = "Select name, registration_date, membership_type, score FROM WorkoutUsers ORDER BY score DESC";
 	$result = performQuery($db, $query);
-	print("<fieldset><legend>Group Members</legend>");
-	print("<table>");
+	print("<fieldset><h2 style='text-align: center'>Group Members</h2>");
+	print("<table align=center>");
 	print("<tr><th>Score</th><th>Name</th><th>Member Since</th><th>Level</th></tr>");
 
    	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
@@ -48,9 +48,9 @@ session_start();
    		$reg_date = $row['registration_date'];
    		$level = $row['membership_type'];
 
-   		print("<td>". $score . "</td>");
+   		print("<td id='odd'>". $score . "</td>");
    		print("<td>". $name . "</td>");
-   		print("<td>". $reg_date . "</td>");
+   		print("<td id='odd'>". $reg_date . "</td>");
    		print("<td>". $level . "</td>");
    		print("</tr>");
 	}
@@ -59,30 +59,4 @@ session_start();
 </fieldset>
 <?php
   }
-?>
-
-	</body>
-	</html>
-
-<?php
-
-
-//______________________ DB CONNECT FUNCTIONS ________________________________________
-function connectToDB($dbname){
-	$dbc= @mysqli_connect("localhost", "pridotka", "2SSMncyC", $dbname) or //password = zr6eRbCP or 2SSMncyC
-					die("Connect failed: ". mysqli_connect_error());
-	return $dbc;
-}
-function disconnectFromDB($dbc, $result){
-	mysqli_free_result($result);
-	mysqli_close($dbc);
-}
-
-function performQuery($dbc, $query){
-	//echo "My query is >$query< <br>";
-	$result = mysqli_query($dbc, $query) or die("bad query".mysqli_error($dbc));
-	return $result;
-}
-
-
 ?>
