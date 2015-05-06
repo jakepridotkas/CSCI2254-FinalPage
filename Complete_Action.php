0d7a35f30eb7cdf7da700ea4442be562731b26fd
pridotka <pridotka@bc.edu>
2015-05-06 12:15:08 -0400

    Create Complete_Action.php

---
+++ Complete_Action.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Workout Completed!</title>
+	<link rel="stylesheet" href="finalProject.css" />
+</head>
+<body>
+<header>
+		<div class="navBar">
+			<ul>
+				<li class="home"><a href="http://cscilab.bc.edu/~pridotka/project/login.php">Login</a></li>
+				<li class="workoutPlanner"><a href="http://cscilab.bc.edu/~mccormky/FinalProject/selectWorkouts.php">Workout Planner</a></li>
+				<li><a href="http://cscilab.bc.edu/~mccormky/FinalProject/workoutArticles.php">Workout Articles</a></li>
+				<li><a href="http://cscilab.bc.edu/~pridotka/project/leaderboard.php">Leader Board</a></li>
+			</ul>
+		</div>
+	</header>
+<?php
+if(isset($_POST["workoutCompleted"])){
+		handle_points();   // If join button is clicked
+	}
+	//_____________________________HANDLE JOIN_______________________________________________________
+	function handle_points(){
+			if ( $_SESSION["Count"] == "1" ) {
+				$message = "You aint that swole!  Login again later to complete another workout...";
+				echo "<script type='text/javascript'>alert('$message');</script>";
+				echo "<script>window.location = 'http://cscilab.bc.edu/~mccormky/FinalProject/selectWorkouts.php'</script>";
+				exit();
+			} else {
+				$_SESSION["Count"] = "1";
+				$Time = $_POST['time'];
+				$name = $_SESSION["UserName"];
+				printf("<fieldset>
+						<legend><b>Your Results!</b></legend>
+						For completing a $Time minute workout, you have recieved $Time points! <br><br>");
+				printf("Go to the leaderboard page to see your updated score! </fieldset>");
+
+				$dbname = connectToDB("pridotka");
+				$query = "UPDATE WorkoutUsers SET score = score + $Time WHERE name ='$name'";
+				performQuery($dbname, $query);
+			}
+			}
+//______________________ DB CONNECT FUNCTIONS ________________________________________
+function connectToDB($dbname){
+	$dbc= @mysqli_connect("localhost", "pridotka", "2SSMncyC", $dbname) or //password = zr6eRbCP or 2SSMncyC
+					die("Connect failed: ". mysqli_connect_error());
+	return $dbc;
+}
+function disconnectFromDB($dbc, $result){
+	mysqli_free_result($result);
+	mysqli_close($dbc);
+}
+function performQuery($dbc, $query){
+	//echo "My query is >$query< <br>";
+	$result = mysqli_query($dbc, $query) or die("bad query".mysqli_error($dbc));
+	return $result;
+}
+	//_____________________________________________________________________________________
+	?>
+
+</body>
+
+</html>
